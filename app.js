43ba738c0a49dd0cd245e7f1553d28e65c5ef5db
Wouter <tuvokki@gmail.com>
2015-06-01 23:28:50 +0200

    advance

--- app.js
+++ app.js
@@ -2,18 +2,30 @@ var bogart = require('bogart')
     ,path  = require('path');
 
 var viewEngine = bogart.viewEngine('mustache', path.join(bogart.maindir(), 'views'));
+var root = require("path").join(__dirname, "public");
 
 var router = bogart.router();
 router.get('/', function(req) { 
-  return viewEngine.respond('index.html', { locals: { description: 'This is content' } });
+  var article = { locals: { title: "first blog", body: "blog body" } };
+
+  return viewEngine.respond('index.html', article);
 });
 
 router.get('/:name', function(req) {
       return 'hello '+req.params.name;
 });
 
+router.get('/posts/new', function(req) {
+  return viewEngine.respond('new-post.html', {
+    locals: {
+      title: 'New Post'
+    }
+  })
+});
+
 var app = bogart.app();
 app.use(bogart.batteries({ secret: 'xGljGo7f4g/a1QveU8VRxhZP5Hwi2YWelejBq5h4ynM'})); // A batteries included JSGI stack including streaming request body parsing, session, flash, and much more.
+app.use(bogart.middleware.directory(root));
 app.use(router); // Our router
 
 app.start(9981);
