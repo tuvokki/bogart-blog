5223855d204b59e9f553641cf6f3e667aa70c253
Wouter <tuvokki@gmail.com>
2015-06-02 13:00:59 +0200

    Advance more

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ var bogart = require('bogart')
 
 var viewEngine = bogart.viewEngine('mustache', path.join(bogart.maindir(), 'views'));
 var root = require("path").join(__dirname, "public");
-
 var router = bogart.router();
+var nano = require('nano')('https://couchdb-f0fd27.smileupps.com');
+
 router.get('/', function(req) { 
-  var article = { locals: { title: "first blog", body: "blog body" } };
+  var article = { locals: { title: "header title", body: "A body that consists of a lot of things." } };
 
   return viewEngine.respond('index.html', article);
 });
@@ -21,6 +22,23 @@ router.get('/posts/new', function(req) {
       title: 'New Post'
     }
   })
+});
+
+router.post('/posts', function(req) {
+  var post = req.params;
+  post.type = 'post';
+
+  var articles = nano.db.use('articles');
+
+  articles.insert(post, function(err, body, header) {
+    if (err) {
+      console.log('[articles.insert] ', err.message);
+      return;
+    }
+    console.log('you have inserted the body: ', body)
+    console.log(body);
+    return bogart.redirect('/posts');
+  });
 });
 
 var app = bogart.app();
