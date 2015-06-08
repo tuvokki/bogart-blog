ef11648cfcce44790573fa2f479f87b611f18892
Wouter <tuvokki@gmail.com>
2015-06-08 16:24:11 +0200

    Get title from posts

--- app.js
+++ app.js
@@ -26,9 +26,14 @@ router.post('/posts', function(req) {
 
   var articles = nano.db.use('articles');
 
+  var slug = req.params.title.replace(/[^a-zA-Z0-9\s]/g,"");
+  slug = slug.toLowerCase();
+  slug = slug.trim();
+  slug = slug.replace(/\s/g,'-');
+
   var insert_article = bogart.promisify(articles.insert);
 
-  return insert_article(post).then(function(data) {
+  return insert_article(post, slug).then(function(data) {
     console.log('you have inserted the body: ', data)
     return bogart.redirect('/posts');
   });
@@ -37,9 +42,9 @@ router.post('/posts', function(req) {
 router.get('/posts', function(req) {
   var articles = nano.db.use('articles');
 
-  var readlist = bogart.promisify(articles.list);
+  var articlelist = bogart.promisify(articles.view);
 
-  return readlist().then(function(data) {
+  return articlelist('article_list', 'articleView').then(function(data) {
     console.log(data);
     return viewEngine.respond('posts.html', {
       locals: {
@@ -49,6 +54,52 @@ router.get('/posts', function(req) {
     });
   });
   console.log('render');
+});
+
+router.get('/posts2/:id', function(req) {
+  console.log('id', req.params.id);
+  var articles = nano.db.use('articles');
+
+  var articlelist = bogart.promisify(articles.view);
+
+  return articlelist('article_list', 'fullArticleView').then(function(data) {
+    console.log(data);
+    return viewEngine.respond('posts.html', {
+      locals: {
+        title: 'all posts',
+        postlist: data.rows
+      }
+    });
+  });
+});
+
+router.get('/posts/:id', function(req) {
+  console.log('id', req.params.id);
+
+  // data = {
+  //   title: req.params.id,
+  //   body: 'This is the body ...'
+  // };
+  // return viewEngine.respond('post.html', {
+  //   locals: {
+  //     title: data.title,
+  //     body: data.body
+  //   }
+  // });
+  var articles = nano.db.use('articles');
+
+  var articlelist = bogart.promisify(articles.get);
+
+  return articlelist(req.params.id).then(function(data) {
+    console.log(data);
+    return viewEngine.respond('post.html', {
+      locals: {
+        title: data.title,
+        body: data.body
+      }
+    });
+  });
+
 });
 
 var app = bogart.app();
